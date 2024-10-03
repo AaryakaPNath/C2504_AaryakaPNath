71565d9470c5e8bc5adbcb5b944c4a6478a501ec
AaryakaPNath <90920105+AaryakaPNath@users.noreply.github.com>
2024-10-03 09:14:26 +0530

    Add files via upload

---
+++ Phase2/CounterConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CounterProject
+{
+    static class CounterConfig
+    {
+        public static CounterWindow FrmCounter { get; set; }
+        public static CounterViewModel VueModel { get; set; }
+        static CounterConfig()
+        {
+            VueModel = new CounterViewModel();
+            FrmCounter = new CounterWindow();
+        }
+    }
+}
